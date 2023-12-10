dc6ce6866dca04827d6e1e30e4d7a95e14aa27ff
Satish Mishra <36122092+ZicsX@users.noreply.github.com>
2023-12-10 12:30:32 +0530

    beh!!

--- data/hindibc2/prepare.py
+++ data/hindibc2/prepare.py
@@ -6,10 +6,11 @@ from tqdm import tqdm
 import numpy as np
 import tiktoken
 from datasets import load_dataset # huggingface datasets
+from multiprocessing import cpu_count
 
 # number of workers in .map() call
 # good number to use is ~order number of cpu cores // 2
-num_proc = 8
+num_proc = cpu_count()
 
 # number of workers in load_dataset() call
 # best number might be different from num_proc above as it also depends on NW speed.
