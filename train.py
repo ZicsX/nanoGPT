8ed5fac179140cd91115b8b1cf0dda25a234fe1a
Satish Mishra <zicsxone@gmail.com>
2024-01-30 00:42:05 +0530

    bias test

--- train.py
+++ train.py
@@ -33,7 +33,7 @@ from model import GPTConfig, GPT
 # default config values designed to train a gpt2 (124M) on OpenWebText
 # I/O
 out_dir = 'out'
-eval_interval = 1000
+eval_interval = 10
 log_interval = 1
 eval_iters = 200
 eval_only = False # if True, script exits right after the first eval
@@ -45,19 +45,19 @@ wandb_project = 'owt'
 wandb_run_name = 'gpt2' # 'run' + str(time.time())
 # data
 dataset = 'compiled'
-gradient_accumulation_steps = 5 * 8 * 6 # used to simulate larger batch sizes
+gradient_accumulation_steps = 1 # used to simulate larger batch sizes
 batch_size = 12 # if gradient_accumulation_steps > 1, this is the micro-batch size
 block_size = 1024
 # model
 #config for 124m paras
-# n_layer=12
-# n_head=12
-# n_embd=768
+n_layer=12
+n_head=12
+n_embd=768
 
 # config for 512m paras 
-n_layer = 24
-n_head = 16
-n_embd = 1280
+# n_layer = 24
+# n_head = 16
+# n_embd = 1280
 
 # #config 774m paras
 # n_layer = 36
@@ -65,10 +65,10 @@ n_embd = 1280
 # n_embd = 1280
 
 dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
-bias = False # do we use bias inside LayerNorm and Linear layers?
+bias = True # do we use bias inside LayerNorm and Linear layers?
 # adamw optimizer
 learning_rate = 6e-4 # max learning rate
-max_iters = 600000 # total number of training iterations
+max_iters = 100 # total number of training iterations
 weight_decay = 1e-1
 beta1 = 0.9
 beta2 = 0.95
@@ -76,7 +76,7 @@ grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
 # learning rate decay settings
 decay_lr = True # whether to decay the learning rate
 warmup_iters = 2000 # how many steps to warm up for
-lr_decay_iters = 600000 # should be ~= max_iters per Chinchilla
+lr_decay_iters = 100 # should be ~= max_iters per Chinchilla
 min_lr = 6e-5 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
 # DDP settings
 backend = 'nccl' # 'nccl', 'gloo', etc.
